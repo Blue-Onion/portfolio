71375c6113aaf867146c2221dfcf0a417f76a0c6
Blue-Onion <rawatadityasingh963@gmail.com>
2025-10-19 10:53:49 +0530

    feat: enhance styling and layout with new components and animations
    - Updated globals.css to include new color variables and dark mode support.
    - Refactored layout.js to integrate Particles background and updated font imports.
    - Enhanced page.jsx with a new layout structure and added buttons for social links.
    - Introduced Particles component for animated background effects.
    - Added Button component with variants for consistent styling.
    - Updated package.json and package-lock.json with new dependencies for UI components.
    - Created components.json for component configuration.

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,55 @@
 import Robo from "@/components/Robo";
-
+import { Button } from "@/components/ui/button";
+import { File, Github, Linkedin } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <main className="w-screen h-screen bg-black">
-    
-    <Robo/>
-  
-   </main>
+    <main className="w-screen max-w-6xl mx-auto min-h-screen ">
+      <section className="grid grid-cols-2 gap-3">
+        <div className="flex items-start mt-28 justify-center h-full w-full">
+          <div className="flex flex-col justify-start p-5 gap-4  items-start">
+            <span className="text-muted-foreground text-lg">Hi,I am </span>
+            <h1 className="text-white text-9xl font-semibold  h-full ">
+              Aditya Rawat
+            </h1>
+            <p className="text-muted-foreground">Devops Enthusiast</p>
+            <div className="flex flex-col gap-3">
+              <Button variant={"secondary"}>
+                <span className="flex">
+                  <File className="w-5 h-5 mr-2" />
+                  Resume
+                </span>
+              </Button>
+              <div className="flex gap-2">
+                <Link href={"https://github.com/Blue-Onion"}>
+                <Button variant={""}>
+                  <span className="flex items-center justify-center">
+                    <Github className="w-5 h-5 mr-2" />
+                Github
+                  </span>
+                </Button>
+                </Link>
+                <Button variant={"https://www.linkedin.com/in/aditya-singh-rawat-0a7416326/"}>
+                  <span className="flex">
+                    <Linkedin className="w-5 h-5 mr-2" />
+                    Linkedin
+                  </span>
+                </Button>
+                <Button variant={""}>
+                  <span className="flex">
+                    <File className="w-5 h-5 mr-2" />
+                    Hire Me
+                  </span>
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <Robo />
+        </div>
+      </section>
+    </main>
   );
 }
--- components/Robo.jsx
+++ components/Robo.jsx
@@ -3,12 +3,12 @@ import Spline from '@splinetool/react-spline';
 
 export default function Robo() {
   return (
-    <main>
-      <div className="w-screen h-screen overflow-hidden relative">
+    <mai className="w-screen h-screen flex">
+      <div className="w-1/2  overflow-hidden relative">
         <div className="absolute -top-[10%] -left-[10%] w-[120%] h-[120%] overflow-hidden">
           <Spline scene="https://prod.spline.design/0FmzagSxHExthxG8/scene.splinecode"/>
         </div>
       </div>
-    </main>
+    </mai>
   );
 }
